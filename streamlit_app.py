5fb040147c0f1742ba90375e55bea7464f1060af
Armellectx <56161210+Armellectx@users.noreply.github.com>
2023-12-07 23:54:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,20 @@ import numpy as np
 from PIL import Image
 import cv2
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from io import BytesIO
+import os
 
-@st.cache_data()
-def load():
-    model = ResNet50(weights='imagenet', include_top=True)
-    #model_path = "best_model.h5"
-    #model = load_model(model_path, compile=False)
-    return model
 
 # Chargement du model
-model = load()
+saved_w = "premiers_poids.h5"
+base_model = ResNet50(weights='imagenet', include_top=False)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(2, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+model.load_weights(saved_w)
 
-
-def predict(upload):
+def predict_old(upload):
 
     img = Image.open(upload)
     img = np.asarray(img)
@@ -35,20 +36,33 @@ def predict(upload):
 
 
 
-st.title("Poubelle Intelligente")
+st.title("Reconnaissance d'animaux sauvages")
 
-upload = st.file_uploader("Chargez l'image de votre objet",
+upload = st.file_uploader("Chargez l'image que vous souhaitez détecter",
                            type=['png', 'jpeg', 'jpg'])
 
 c1, c2 = st.columns(2)
 
 if upload:
-    rec = predict(upload)
-    prob_recyclable = rec * 100      
-    prob_organic = (1-rec)*100
+    
+    img = Image.open(BytesIO(upload.getvalue()))
+    
+    img = img.resize((224, 224))
+    img = np.asarray(img)
+    img = np.expand_dims(img, axis=0)
 
-    c1.image(Image.open(upload))
-    if prob_recyclable > 50:
-        c2.write(f"Je suis certain à {prob_recyclable:.2f} % que l'objet est recyclable")
+    #traitement de l'image
+    #img_processed = preprocess(img)
+
+    # predictions
+    rec = model.predict(img)
+    
+    predicted_classes = np.argmax(rec, axis=1)
+    if 0.1 <= rec[0][0] <= 0.9:
+        c2.write("No Wild boar or Deer detected on this picture")
+    elif predicted_classes == 0: 
+        c2.write("It is a wild boar. Il s'agit d'un sanglier (wild boar)")
     else:
-        c2.write(f"Je suis certain à {prob_organic:.2f} % que l'objet n'est pas recyclable")
+        c2.write("It is a deer. Il s'agit d'un cerf (deer)")
+    c1.image(Image.open(upload))
+    
